466911e7214e543aa6f2111e5c429a716c036dfb
Alex <alsergeich@mail.ru>
2015-05-12 22:13:38 +0300

    SecondNewWar

--- app/src/main/java/ru/loftschool/moneytrackerloft/MainActivity.java
+++ app/src/main/java/ru/loftschool/moneytrackerloft/MainActivity.java
@@ -6,8 +6,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -16,27 +21,35 @@ public class MainActivity extends ActionBarActivity {
     private TransactionAdapter transactionAdapter;
     List<Transaction> data = new ArrayList<>();
 
+    public MainActivity() {
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        List<Transaction> adapterData = getDataList();
+        List<Transaction> adapterData = getTransactions();
         transactionAdapter = new TransactionAdapter(this, adapterData);
 
         listView = (ListView) findViewById(R.id.listview);
         listView.setAdapter(transactionAdapter);
     }
 
-    private List<Transaction> getDataList() {
-        data.add(new Transaction("Telephone", "2000", "04.05 .2015"));
-        data.add(new Transaction("Visky", "1000", "04.05 .2015"));
-        data.add(new Transaction("Coffy", "800", "04.05 .2015"));
-        data.add(new Transaction("Tea", "300", "04.05 .2015"));
-        data.add(new Transaction("Bred", "20", "04.05 .2015"));
 
+    private String date_format = "dd-MM-yyyy";
+
+    private List<Transaction> getTransactions() {
+
+        DateFormat df = new SimpleDateFormat(date_format, new Locale("ru"));
+        Date now_calendar = Calendar.getInstance().getTime();
+        String now = df.format(now_calendar);
+        data.add(new Transaction("Huawei", "9800", now));
+        data.add(new Transaction("SamsungS3", "13000", now));
+        data.add(new Transaction("T-shirt", "300", now));
+        data.add(new Transaction("Jeans", "1500", now));
+        data.add(new Transaction("Printer", "4500", now));
         return data;
     }
-
-
 }
